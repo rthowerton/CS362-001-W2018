8ed005d3afffa05213da577aeb295099a566621d
rthowerton <howertor@oregonstate.edu>
2018-01-28 23:48:52 -0800

    This is really bad process.
    Created a bunch of stuff and added it to local master.

--- projects/howertor/assignment-2/Calendar/src/test/java/calendar/ApptTest.java
+++ projects/howertor/assignment-2/Calendar/src/test/java/calendar/ApptTest.java
@@ -40,7 +40,45 @@ public class ApptTest {
 
 	 @Test
 	  public void test02()  throws Throwable  {
-		 
+		 int startHour=15;
+		 int startMinute=00;
+		 int startDay=01;
+		 int startMonth=02;
+		 int startYear=2017;
+		 String title="Monthly Standup";
+		 String description="This is the monthly standup meeting.";
+		 //Construct a new Appointment object with the initial data
+		 Appt appt = new Appt(startHour,
+				 startMinute ,
+				 startDay ,
+				 startMonth ,
+				 startYear ,
+				 title,
+				 description);
+		 //assertions
+		 assertTrue(appt.getValid());
+		 appt.setStartHour(14);
+		 assertEquals(14, appt.getStartHour());
+		 appt.setStartMinute(30);
+		 assertEquals(30, appt.getStartMinute());
+		 appt.setStartDay(03);
+		 assertEquals(03, appt.getStartDay());
+		 appt.setStartMonth(03);
+		 assertEquals(03, appt.getStartMonth());
+		 appt.setStartYear(2018);
+		 assertEquals(2018, appt.getStartYear());
+		 appt.setTitle("Weekly Standup");
+		 assertEquals("Weekly Standup", appt.getTitle());
+		 appt.setDescription("This is the weekly standup meeting.");
+		 assertEquals("This is the weekly standup meeting.", appt.getDescription());
+		 //int[] recurDays = new int[7];
+		 appt.setRecurrence(null, Appt.RECUR_BY_WEEKLY, 1, Appt.RECUR_NUMBER_FOREVER);
+		 assertTrue(appt.isRecurring());
+		 assertArrayEquals(new int[0], appt.getRecurDays());
+		 assertEquals(Appt.RECUR_BY_WEEKLY, appt.getRecurBy());
+		 assertEquals(1, appt.getRecurIncrement());
+		 assertEquals(Appt.RECUR_NUMBER_FOREVER, appt.getRecurNumber());
+		 assertEquals("\t3/3/2018 at 2:30pm ,Weekly Standup, This is the weekly standup meeting.\n", appt.toString());
 	 }
 //add more unit tests as you needed
 	
