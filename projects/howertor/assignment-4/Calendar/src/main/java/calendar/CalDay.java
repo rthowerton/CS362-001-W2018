ec696e97512ff574e2391b37882a533a689daba5
rthowerton <howertor@oregonstate.edu>
2018-03-09 19:31:53 -0800

    Reverted to Buggy Code
    For use by hutchico

--- projects/howertor/assignment-4/Calendar/src/main/java/calendar/CalDay.java
+++ projects/howertor/assignment-4/Calendar/src/main/java/calendar/CalDay.java
@@ -111,10 +111,10 @@ public class CalDay {
 	
 	/** Sets appts */
 	private void setAppts(LinkedList<Appt> appts) {
-		if(appts!=null)
+		if(appts==null)
 				this.appts = appts;
 		
-		if(appts!=null&&appts.size()==0)
+		if(appts==null&&appts.size()==0)
 			this.appts = appts;
 	}
 	
@@ -180,7 +180,7 @@ public class CalDay {
 		         sb.append(element + " ");
 		      }
 		  
-//			sb.append(this.appts);
+			sb.append(this.appts);
 			sb.append("\n");
 		}
        	 return sb.toString();
